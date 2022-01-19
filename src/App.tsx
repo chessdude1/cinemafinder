db34b2aba25ac9e12f231d38018cb14be00a4690
Albina Shaikhutdinova <ya.shalbina@gmail.com>
2022-01-19 20:01:14 +0300

    feat: add lint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { StartPage } from './Views/MainPage/StartPage';
+import { SearchPage } from './Views/SearchPage/SearchPage';
+
 import './App.scss';
 
 export function App() {
   return (
-    <div />
+    <>
+      <header>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/search'>Search</NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<StartPage />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='*' element={<SearchPage />} />
+      </Routes>
+    </>
   );
 }
---
+++ src/Views/SearchPage/SearchPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SearchPage() {
+  return (
+    <div>
+      <h2>Search</h2>
+    </div>
+  );
+}
