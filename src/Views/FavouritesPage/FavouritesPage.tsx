caabf812d606902824e8b74921ef6f99e6d5b958
chessdude1 <mitia.platonow2011@yandex.ru>
2022-01-22 00:53:14 +0300

    feat: add basic redux implementation

--- src/Views/FavouritesPage/FavouritesPage.tsx
+++ src/Views/FavouritesPage/FavouritesPage.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useTypedSelector } from '../../Hooks/useTypedSelector';
+import { FavouritePageActions } from '../../redux/FavouritesPageRedux/FavouritePageActions';
 
 export function FavouritesPage() {
-  return <h2>Favorite movies list</h2>;
+  const number = useTypedSelector((store) => store.FavouritesPageReducer.number);
+  const dispatch = useDispatch();
+  function increment() {
+    dispatch(FavouritePageActions.Increment());
+  }
+  function decrement() {
+    dispatch(FavouritePageActions.Decrement());
+  }
+  return (
+    <div>
+
+      <h2>{number}</h2>
+      <button type='button' aria-label='Save' onClick={() => { increment(); }} />
+      <button type='button' aria-label='Save' onClick={() => { decrement(); }} />
+    </div>
+  );
 }
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,7 +1,11 @@
-import { combineReducers, createStore } from "redux";
-import { FavouritesPageReducer } from "./FavouritesPageRedux/FavouritesPageReducer";
-let reducers = combineReducers({
-  FavouritesPage: FavouritesPageReducer,
+import { combineReducers, createStore } from 'redux';
+import { FavouritesPageReducer } from './FavouritesPageRedux/FavouritesPageReducer';
+
+const reducers = combineReducers({
+  FavouritesPageReducer,
 });
 
-const store = createStore(reducers);
+export const store = createStore(reducers);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = ReturnType<typeof store.dispatch>
