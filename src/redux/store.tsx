088ac5c5a8e4b0dfc6de38170accab8ff62e18c5
chessdude1 <mitia.platonow2011@yandex.ru>
2022-01-22 16:14:00 +0300

    refactor: add redux-devtools

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,4 +1,6 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import {
+  applyMiddleware, combineReducers, createStore, compose,
+} from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 import { FavouritesPageReducer } from './FavouritesPageRedux/FavouritesPageReducer';
 import { AuthPageReducer } from './AuthPageRedux/AuthPageReducer';
@@ -9,7 +11,18 @@ const reducers = combineReducers({
   AuthPageReducer,
 });
 
-export const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = ReturnType<typeof store.dispatch>
