2c6c8c7d9179f17d2616269bb8b30f36d3761e20
dosjcq <nagibin.artyom1999@gmail.com>
2022-01-27 20:35:04 +0300

    feat: add base layout for registration page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SearchPage } from './Views/SearchPage/SearchPage';
 import { PageNotFound } from './Views/ErrorPage/PageNotFound';
 import { RegistrationPage } from './Views/RegistrationPage/RegistrationPage';
 import { AccountPage } from './Views/AccountPage/AccountPage';
+import Header from './Layouts/Header';
 
 import './App.scss';
 import { FavouritesPageAux } from './Views/FavouritesPage/FavouritesPageAux';
@@ -13,17 +14,19 @@ import { SettingsPage } from './Views/AccountPage/SettingsPage/SettingsPage';
 
 export function App() {
   return (
-    <Routes>
-      <Route path='/' element={<StartPage />}>
-        <Route path='search' element={<SearchPage />} />
-        <Route path='registration' element={<RegistrationPage />} />
-        <Route path='movie' element={<MoviePage />} />
-        <Route path='favourites' element={<FavouritesPageAux />} />
-        <Route path='account' element={<AccountPage />}>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<StartPage />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='/registration' element={<RegistrationPage />} />
+        <Route path='/movie' element={<MoviePage />} />
+        <Route path='/favourites' element={<FavouritesPageAux />} />
+        <Route path='/account' element={<AccountPage />}>
           <Route path='settings' element={<SettingsPage />} />
         </Route>
-      </Route>
-      <Route path='*' element={<PageNotFound />} />
-    </Routes>
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </>
   );
 }
--- src/Views/RegistrationPage/RegistrationPage.tsx
+++ src/Views/RegistrationPage/RegistrationPage.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+
 export function RegistrationPage() {
-  return <h2>Sign Up</h2>;
+  return (
+    <Container maxWidth='md'>
+      <h2>Create free account</h2>
+      <Box
+        component='form'
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          '& > :not(style)': { m: 1, width: '60%' },
+        }}
+        noValidate
+        autoComplete='off'
+      >
+        <TextField id='outlined-basic' label='Name' variant='filled' />
+        <TextField id='filled-basic' label='Email' variant='filled' />
+        <TextField id='standard-basic' label='Password' variant='filled' />
+      </Box>
+    </Container>
+  );
 }
