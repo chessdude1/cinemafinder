7df2450132bbc2c04504ff42fde20594c08cf682
chessdude1 <mitia.platonow2011@yandex.ru>
2022-02-14 22:51:25 +0300

    feat: add default buttons

---
+++ src/Common/UI/CustomFilterButton/CustomFilterButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export function CustomFilterButton() {
+  return (
+    <Button variant='outlined' href='#outlined-buttons'>
+      Link
+    </Button>
+  );
+}
