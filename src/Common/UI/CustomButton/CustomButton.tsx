1be4306b619b1540fdef5a0b3b06de7b25cb7095
chessdude1 <mitia.platonow2011@yandex.ru>
2022-02-15 11:26:48 +0300

    fix: delete unused imports

--- src/Common/UI/CustomButton/CustomButton.tsx
+++ src/Common/UI/CustomButton/CustomButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@mui/material/Button';
 import './CustomButtonStyles.scss';
 import filter from '../../../Assets/img/Button/filter.png';
