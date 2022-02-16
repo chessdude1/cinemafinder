287fd1de8ff492777ab88f4cc1fb8466a12248a2
Albina Shaikhutdinova <ya.shalbina@gmail.com>
2022-02-16 19:35:36 +0300

    fix: movie sort

--- src/Views/SearchPage/Filters/SortOrder.tsx
+++ src/Views/SearchPage/Filters/SortOrder.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { SetStateAction } from 'react';
 import CustomRadioInput from '../../../Common/UI/CustomRadioInput';
 import { CustomSelect } from '../../../Common/UI/CustomSelect/CustomSelect';
-import { providerFilter, watchProvider } from '../SearchQueryTypes';
+import { CustomSingleSelect } from '../../../Common/UI/CustomSelect/CustomSingleSelect';
+import { providerFilter, SORT_ORDER, watchProvider } from '../SearchQueryTypes';
 
 export interface SortOrderType {
   setSortOrder: React.Dispatch<React.SetStateAction<string>>;
@@ -9,18 +10,21 @@ export interface SortOrderType {
   sortsList: string[];
 }
 export function SortOrder({ setSortOrder, sortOrder, sortsList }: SortOrderType) {
-  function updateFieldChanged(names: string[]) {
-    setSortOrder(names[0]);
+  function getKeyName(value: string) {
+    return Object.entries(SORT_ORDER).find(([key, val]) => val === value)?.[0];
   }
+  function updateFieldChanged(name: string) {
+    setSortOrder(getKeyName(name) as string);
+  }
+
   return (
     <section>
       <div className='sort-options'>
-        <CustomSelect
-          isMultiple={false}
-          checkedArray={[sortOrder]}
-          variants={sortsList.map((order) => order)}
+        <CustomSingleSelect
+          checkedArray={SORT_ORDER[sortOrder as keyof typeof SORT_ORDER]}
+          variants={sortsList.map((order) => SORT_ORDER[order as keyof typeof SORT_ORDER])}
           placeholder='order'
-          handleMultipleSelect={(value: string[]) => {
+          handleSelect={(value: string) => {
             updateFieldChanged(value);
           }}
         />
--- src/Views/SearchPage/SearchQueryTypes.tsx
+++ src/Views/SearchPage/SearchQueryTypes.tsx
@@ -4,7 +4,14 @@ export interface sortOrderTypes {
   rating: boolean;
 }
 export const sortTypes: string[] = ['popularity.desc', 'popularity.asc', 'release_date.desc', 'release_date.asc', 'original_title.desc', 'original_title.asc'];
-
+export enum SORT_ORDER {
+  'popularity.desc' = 'More Popular',
+  'popularity.asc' = 'Less Popular',
+  'release_date.desc' = 'New first',
+  'release_date.asc' = 'Old first',
+  'original_title.asc' = 'Alphabet order',
+  'original_title.desc' = 'Reverse alphabet order',
+}
 export interface IGenre {
   id: number;
   name: string;
