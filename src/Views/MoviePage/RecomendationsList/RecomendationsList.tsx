05566ec8359b401d3a8e1a009654e38f0058d9fb
chessdude1 <mitia.platonow2011@yandex.ru>
2022-02-17 16:51:59 +0300

    feat: button add favorite film

--- src/Views/MoviePage/RecomendationsList/RecomendationsList.tsx
+++ src/Views/MoviePage/RecomendationsList/RecomendationsList.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { AuthPageActions } from '../../../redux/AuthPageRedux/AuthPageActions';
 import { filmResponse } from '../../../Services/ServiceTypes';
 import { MovieCardSmall } from '../../SearchPage/MovieTable/MovieCard/MovieCardSmall';
+import { GetGenresFromIds } from '../../../Auxiliary/GetGenresFromIds';
 import './RecomendationsListStyles.scss';
+import { TranslateGenre } from '../../../Auxiliary/TranslateGenre';
 
 interface IRecomendationsList {
   similarFilms : Array<filmResponse> | undefined;
 }
 
 export function RecomendationsList({ similarFilms } : IRecomendationsList) {
-  const dispatch = useDispatch();
-
-  const AddFilmToUserFavourite = (filmId : string) => {
-    dispatch(AuthPageActions.SetFavoriteFilm(filmId));
-  };
-
   return (
     <div className='recomendation-list__wrapper'>
       {similarFilms?.map((similarFilm) => (
-        <div className="recomendation-list__movie">
+        <div key={similarFilm.id} className='recomendation-list__movie'>
           <MovieCardSmall
             originalTitle={similarFilm.original_title}
-            genre='Экшн'
-            year='2001'
+            genre={TranslateGenre((GetGenresFromIds(similarFilm.genre_ids))).join(' ')}
+            year={String((new Date(similarFilm.release_date)).getFullYear())}
             posterPath={similarFilm.poster_path}
             id={similarFilm.id}
-            key={similarFilm.id}
           />
         </div>
       ))}
