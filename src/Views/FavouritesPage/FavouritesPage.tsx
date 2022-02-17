ad602eeb52fe320fb02fcc3209c2cc26cb6fa8df
chessdude1 <mitia.platonow2011@yandex.ru>
2022-02-17 18:26:01 +0300

    feat: add empty favotite film component

--- src/Views/FavouritesPage/FavouritesPage.tsx
+++ src/Views/FavouritesPage/FavouritesPage.tsx
@@ -11,6 +11,7 @@ import './FavouritePageStyles.scss';
 import { CustomButton } from '../../Common/UI/CustomButton/CustomButton';
 import { TemporaryDrawer } from '../../Common/UX/Drawer/Drawer';
 import { UserNotAuthorizedCard } from '../../Common/UX/UserNotAuthorizedCard/UserNotAuthorizedCard';
+import { EmptyFavouriteFilmsCard } from '../../Common/UX/EmptyFavouriteFilmsCard/EmptyFavouriteFilmsCard';
 
 interface FavouritesPageType {
   favoriteFilms : Array<FavouriteFilm>,
@@ -49,92 +50,99 @@ export function FavouritesPage({
     <main className='favourite-page'>
       { isLogin
         ? (
-          <>
-            <div className='favourite-page__head'>
-              <Typography sx={{ fontWeight: '600', marginTop: '4.7rem' }} variant='h2'>Избранное</Typography>
-              <CustomButton type='button' onClick={() => setDrawer(true)} variant='outlined'>Фильтры</CustomButton>
-            </div>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
-              {favoriteFilms.map((favoriteFilm) => (
-                <FavoriteFilmCard
-                  key={favoriteFilm.id + Math.random()}
-                  releaseDate={favoriteFilm.releaseDate}
-                  watchProviders={favoriteFilm.watchProviders}
-                  voteAverage={favoriteFilm.voteAverage}
-                  genres={favoriteFilm.genres}
-                  id={favoriteFilm.id}
-                  posterPath={favoriteFilm.posterPath}
-                  originalTitle={favoriteFilm.originalTitle}
-                />
-              ))}
-            </Box>
+          <div>
+            {
+            favoriteFilms.length > 0
+              ? (
+                <>
+                  <div className='favourite-page__head'>
+                    <Typography sx={{ fontWeight: '600', marginTop: '4.7rem' }} variant='h2'>Избранное</Typography>
+                    <CustomButton type='button' onClick={() => setDrawer(true)} variant='outlined'>Фильтры</CustomButton>
+                  </div>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+                    {favoriteFilms.map((favoriteFilm) => (
+                      <FavoriteFilmCard
+                        key={favoriteFilm.id + Math.random()}
+                        releaseDate={favoriteFilm.releaseDate}
+                        watchProviders={favoriteFilm.watchProviders}
+                        voteAverage={favoriteFilm.voteAverage}
+                        genres={favoriteFilm.genres}
+                        id={favoriteFilm.id}
+                        posterPath={favoriteFilm.posterPath}
+                        originalTitle={favoriteFilm.originalTitle}
+                      />
+                    ))}
+                  </Box>
 
-            <TemporaryDrawer isDrawerOpen={isDrawerOpen} setDrawer={setDrawer}>
-              <div className='favourite-page__filters'>
-                <div className='filters__rating-filter'>
-                  <CustomSlider
-                    onChange={(value) => {
-                      setRatingFilterValue(
-                        value,
-                      );
-                    }}
-                    step={1}
-                    min={1}
-                    max={10}
-                    value={ratingFilterValue}
-                    title='Рейтинг'
-                  />
-                </div>
-                <Divider />
-                <div className='filters__year-filter'>
-                  <CustomSlider
-                    onChange={(value) => {
-                      setYearFilterValue(
-                        value,
-                      );
-                    }}
-                    step={10}
-                    min={1900}
-                    max={2022}
-                    value={yearFilterValue}
-                    title='Год'
-                  />
-                </div>
-                <Divider />
-                <div className='filters__options'>
-                  <Typography variant='h4' sx={{ fontWeight: '600', marginTop: '2rem', marginBottom: '0.5rem' }}>Доступность</Typography>
-                  { checkboxsOptions.map((checkbox) => (
-                    <CustomLabeledCheckbox
-                      key={checkbox[0]}
-                      label={`${checkbox[1]}`}
-                      onChange={(isChecked) => {
-                        setOptions({
-                          ...options,
-                          [checkbox[0]]: isChecked,
-                        });
-                      }}
-                    />
-                  ))}
-                </div>
-                <Divider />
-                <div className='filter__genres'>
-                  <Typography variant='h4' sx={{ fontWeight: '600', marginTop: '2rem', marginBottom: '0.5rem' }}>Жанр</Typography>
-                  {checkboxsGenres.map((checkbox) => (
-                    <CustomLabeledCheckbox
-                      key={checkbox[0]}
-                      label={`${checkbox[1]}`}
-                      onChange={(isChecked) => {
-                        setGenres({
-                          ...genres,
-                          [checkbox[0]]: isChecked,
-                        });
-                      }}
-                    />
-                  ))}
-                </div>
-              </div>
-            </TemporaryDrawer>
-          </>
+                  <TemporaryDrawer isDrawerOpen={isDrawerOpen} setDrawer={setDrawer}>
+                    <div className='favourite-page__filters'>
+                      <div className='filters__rating-filter'>
+                        <CustomSlider
+                          onChange={(value) => {
+                            setRatingFilterValue(
+                              value,
+                            );
+                          }}
+                          step={1}
+                          min={1}
+                          max={10}
+                          value={ratingFilterValue}
+                          title='Рейтинг'
+                        />
+                      </div>
+                      <Divider />
+                      <div className='filters__year-filter'>
+                        <CustomSlider
+                          onChange={(value) => {
+                            setYearFilterValue(
+                              value,
+                            );
+                          }}
+                          step={10}
+                          min={1900}
+                          max={2022}
+                          value={yearFilterValue}
+                          title='Год'
+                        />
+                      </div>
+                      <Divider />
+                      <div className='filters__options'>
+                        <Typography variant='h4' sx={{ fontWeight: '600', marginTop: '2rem', marginBottom: '0.5rem' }}>Доступность</Typography>
+                        { checkboxsOptions.map((checkbox) => (
+                          <CustomLabeledCheckbox
+                            key={checkbox[0]}
+                            label={`${checkbox[1]}`}
+                            onChange={(isChecked) => {
+                              setOptions({
+                                ...options,
+                                [checkbox[0]]: isChecked,
+                              });
+                            }}
+                          />
+                        ))}
+                      </div>
+                      <Divider />
+                      <div className='filter__genres'>
+                        <Typography variant='h4' sx={{ fontWeight: '600', marginTop: '2rem', marginBottom: '0.5rem' }}>Жанр</Typography>
+                        {checkboxsGenres.map((checkbox) => (
+                          <CustomLabeledCheckbox
+                            key={checkbox[0]}
+                            label={`${checkbox[1]}`}
+                            onChange={(isChecked) => {
+                              setGenres({
+                                ...genres,
+                                [checkbox[0]]: isChecked,
+                              });
+                            }}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  </TemporaryDrawer>
+                </>
+              ) : <EmptyFavouriteFilmsCard />
+            }
+          </div>
         ) : <UserNotAuthorizedCard />}
     </main>
   );
