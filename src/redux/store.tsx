b373265ccb71922a4fb9b0279b8b040082bbb24f
chessdude1 <mitia.platonow2011@yandex.ru>
2022-03-03 17:12:22 +0300

    fix: delete react dev tools

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore, compose } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 import { FavouritesPageReducer } from './FavouritesPageRedux/FavouritesPageReducer';
 import { AuthPageReducer } from './AuthPageRedux/AuthPageReducer';
@@ -15,15 +15,7 @@ const reducers = combineReducers({
   SearchQueryReducer,
 });
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware)));
+export const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
